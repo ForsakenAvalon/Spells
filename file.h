0baf852e0d628eced9309375062a8feb23b9cd14
Geraint Spackman <forsaken.avalon@gmail.com>
2011-09-02 23:35:22 +0100

    Added Partially Complete String and File handling class TAG:02092011C1

---
+++ file.h
@@ -0,0 +1,41 @@
+#ifndef DFILE_H
+#define DFILE_H
+
+#include "string.h"
+#include <fstream>
+
+class File
+{
+    public:
+        // Constructors
+        File(const char * staticFilePath);
+        File(String & stringFilePath);
+        // Deconstructors
+        ~File();
+
+        int Write();
+        int Write(const char * staticText);
+        int Write(String & stringText);
+
+        int Read();
+        int Read(uint pointer);
+        int Read(char * charDelim);
+
+        static bool DoesFileExist(String & stringFileName);
+        static bool DoesFileExist(const char * staticFileName);
+
+    private:
+        String * Path;
+
+        String * ReadBuffer;
+        String * WriteBuffer;
+
+        int WriteMode;
+
+        bool     IsOpen;
+        bool     IsInit;
+        bool     IsReadBufInit;
+        bool     IsWriteBufInit;
+};
+
+#endif // DFILE_H
