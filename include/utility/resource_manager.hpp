01bde0f9aafdf2ff87124618a89508fb28355374
James Didehvar <james@didehvar.com>
2011-09-04 00:06:53 +0100

    Added more resource types to the resource manager, made folders for resource storage and removed old namespace.

--- include/utility/resource_manager.hpp
+++ include/utility/resource_manager.hpp
@@ -1,6 +1,6 @@
 
-#ifndef UTILITY_RESOURCE_MANAGER
-#define UTILITY_RESOURCE_MANAGER
+#ifndef UTILITY_RESOURCE_MANAGER_H
+#define UTILITY_RESOURCE_MANAGER_H
 
 #include <boost/any.hpp>
 #include <boost/shared_ptr.hpp>
@@ -13,39 +13,44 @@
 //
 #include <SFML/Graphics/Image.hpp>
 typedef boost::shared_ptr<sf::Image> ImageRes; // Used outside of this file for this class.
+#include <SFML/Graphics/Font.hpp>
+typedef boost::shared_ptr<sf::Font> FontRes;
+#include <SFML/Graphics/Shader.hpp>
+typedef boost::shared_ptr<sf::Font> ShaderRes;
+#include <SFML/Audio/SoundBuffer.hpp>
+typedef boost::shared_ptr<sf::Font> SoundBufferRes;
+#include <SFML/Audio/Music.hpp>
+typedef boost::shared_ptr<sf::Font> MusicRes;
 
-namespace Yttrium
+namespace Utility
 {
-	namespace Utility
+	class ResourceManager
 	{
-		class ResourceManager
+	public:
+		typedef std::map<std::string, boost::any> ResourceMap;
+
+		ResourceManager();
+		~ResourceManager();
+
+		template< typename T >
+		boost::shared_ptr<T> GetResource(const std::string &filename)
 		{
-		public:
-			typedef std::map<std::string, boost::any> ResourceMap;
+			ResourceMap::iterator iter = resources_.find(filename);
+			if ( iter != resources_.end() ) // If we have found a resource there is no need to load one.
+				return boost::any_cast<boost::shared_ptr<T>>(iter->second);
 
-			ResourceManager();
-			~ResourceManager();
+			T* raw_resource = LoadResource<T>(filename); // Load a new resource.
+			resources_[filename] = boost::shared_ptr<T>(raw_resource);
 
-			template< typename T >
-			boost::shared_ptr<T> GetResource(const std::string &filename)
-			{
-				ResourceMap::iterator iter = resources_.find(filename);
-				if ( iter != resources_.end() ) // If we have found a resource there is no need to load one.
-					return boost::any_cast<boost::shared_ptr<T>>(iter->second);
+			return boost::any_cast<boost::shared_ptr<T>>(resources_[filename]);
+		}
 
-				T* raw_resource = LoadResource<T>(filename); // Load a new resource.
-				resources_[filename] = boost::shared_ptr<T>(raw_resource);
+	private:
+		template< typename T >
+		T* LoadResource(const std::string &filename);
 
-				return boost::any_cast<boost::shared_ptr<T>>(resources_[filename]);
-			}
-
-		private:
-			template< typename T >
-			T* LoadResource(const std::string &filename);
-
-			ResourceMap resources_;
-		};
-	}
+		ResourceMap resources_;
+	};
 }
 
-#endif // UTILITY_RESOURCE_MANAGER
+#endif // UTILITY_RESOURCE_MANAGER_H
