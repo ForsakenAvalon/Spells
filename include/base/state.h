4badf8b8dd4d397bfb8371196529c3ddb87c9726
James Didehvar <james@didehvar.com>
2011-09-08 21:11:26 +0100

    Documented Base::State.

--- include/base/state.h
+++ include/base/state.h
@@ -16,24 +16,60 @@ namespace States
 
 namespace Base
 {
+	//! \brief An abstract class used by StateManager to control the windows current state
 	class State
 	{
 	public:
 		State( const States::StateList state, Core::Window &window );
 		virtual ~State();
 
+		//! \brief Handles the specific states events
 		virtual void Events( sf::Event &objEvent )	= 0;
+
+		//! \brief Handles the specific states updates
+		//! 
+		//! This includes any draw calls, and is called after events.
+		//!
+		//! \see Events
 		virtual void Update()						= 0;
 
-		virtual void TransitionEnter()				= 0; // Transition for entering this state.
-		virtual void TransitionExit()				= 0; // Transition for exiting this state.
+		//! \brief Called upon entering the state
+		//! 
+		//! \see TransitionExit
+		virtual void TransitionEnter()				= 0;
 
+		//! \brief Called upon exiting the state
+		//! 
+		//! \see TransitionEnter
+		virtual void TransitionExit()				= 0;
+
+
+		//! \brief Returns the states name from the StateList enum
+		//! 
+		//! \see States::StateList
 		States::StateList GetState() { return this->my_state; }
 
 	protected:
-		States::StateList my_state;
-		Core::Window &window;
+		States::StateList my_state; //!< States::StateList enumeration of this state
+		Core::Window &window;		//!< Reference to Core::Window
 	};
 }
 
 #endif // BASE_STATE_H
+
+//! \class Base::State
+//! \ingroup base
+//! 
+//! States are a set of classes that all control different aspects of the game.
+//! Where as one state may control a menu, or an options menu, another could be
+//! in control of the actual game itself; managing entities, physics and more.
+//! 
+//! As an abstract class it is reccomended that all inheriting classes have use
+//! for each abstract function. If the Update() or Events() calls are empty no
+//! drawing will be done to the screen and no events will be processed (unless
+//! they are global).
+//! 
+//! TransitionEnter() and TransitionExit() are mainly provided for cleaning up
+//! memory upon exiting the state and reinitialising memory upon entering it.
+//! As states will not be deconstructed until the end of the program it is
+//! essential that they don't hold lots of memory while unused.
