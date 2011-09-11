f78c8de69ea249d16de27ae152ee6fa8b333b1bb
Geraint Spackman <forsaken.avalon@gmail.com>
2011-09-11 21:25:47 +0800

    Changed main to original version!

--- source/Main.cpp
+++ source/Main.cpp
@@ -7,18 +7,15 @@
 int main()
 {
 	// Initialize Game Window
-	//Core::Window * window;
-	//window = new (std::nothrow) Core::Window;
+	Core::Window * window;
+	window = new (std::nothrow) Core::Window;
 
 	// Assert if initialization succeded
-	//assert(window != NULL && "Cannot create window at main()");
+	assert(window != NULL && "Cannot create window at main()");
 
-	Util::String A("Hello");
-	Util::String B("World!");
+	window->Run();
 
-	std::cout<< (A += B);
-	std::cin.get();
-	//delete window;
+	delete window;
 
 	return 0;
 }
