673d755d9c0dbb4e414738d01aca0360508f5872
James Didehvar <james@didehvar.com>
2011-09-13 19:13:50 +0100

    Removed example code in Main.cpp.

--- source/Main.cpp
+++ source/Main.cpp
@@ -4,48 +4,8 @@
 #include <assert.h>
 #include <iostream>
 
-#include "utility/class_parser.h"
-
-class BasicTest
-{
-public:
-	BasicTest();
-	~BasicTest();
-
-	int x;
-};
-
-BasicTest::BasicTest()
-{
-	x = 5;
-};
-
-BasicTest::~BasicTest()
-{
-};
-
 int main()
 {
-	BasicTest *basic = new BasicTest();
-	Utility::ClassParser *class_parser = new Utility::ClassParser();
-
-	std::string test = "test.dat";
-	if ( !class_parser->WriteClass<BasicTest>(basic, test) )
-	{
-		std::cout << "Couldn't write class!" << std::endl;
-		return 0;
-	}
-
-	BasicTest *basic_2 = new BasicTest();
-	basic_2 = class_parser->ReadClass<BasicTest>(test);
-	
-	assert(basic_2 != NULL && "Basic 2 was null");
-
-	std::cout << "Success! " << basic_2->x << std::endl;
-
-	delete basic;
-	delete basic_2;
-
 	// Initialize Game Window
 	Core::Window * window;
 	window = new (std::nothrow) Core::Window;
