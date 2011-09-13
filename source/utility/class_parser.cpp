3bcc3899dc57ba08270932bbbdf68c4e9775c5d5
James Didehvar <james@didehvar.com>
2011-09-13 19:15:03 +0100

    Removed old code from Utility::ClassParser.

--- source/utility/class_parser.cpp
+++ source/utility/class_parser.cpp
@@ -12,25 +12,4 @@ namespace Utility
 	{
 
 	}
-
-	/*template< typename T >
-	bool ClassParser::WriteClass( T* a_class, std::string &filename )
-	{
-		std::ofstream output(filename, std::ios::binary);
-		output.write(reinterpret_cast< const char* >(a_class), sizeof(*a_class));
-		output.close();
-	}
-
-	template< typename T >
-	T* ClassParser::ReadClass( std::string &filename )
-	{
-		std::ifstream input(filename, std::ios::binary);
-		if ( !input )
-			return false;
-
-		T* ClassHolder();
-
-		input.read(reinterpret_cast< const char* >(ClassHolder), sizeof(*ClassHolder));
-		input.close();
-	}*/
 }
