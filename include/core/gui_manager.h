51d2479e7a671bf1d38e0bd5ce483be98e55c700
James Didehvar <james@didehvar.com>
2011-09-22 05:46:44 +0800

    Documented Core::GUIManager.

--- include/core/gui_manager.h
+++ include/core/gui_manager.h
@@ -26,24 +26,62 @@ namespace Core
 {
 	class Config;
 
+	//! \brief Manages all graphical user interface elements
 	class GUIManager
 	{
 	public:
 		typedef std::map<std::string, GUI::Button*>	ButtonMap;
 		typedef std::map<std::string, GUI::ActionBar*> ActionBarMap;
 
+		//! \brief Constructor
+		//! 
+		//! Creates a GUI manager containing three necessary references.
+		//! 
+		//! \param window			sf::RenderWindow reference
+		//! \param config			Core::Config reference
+		//! \param resource_manager Utility::ResourceManager reference
 		GUIManager( sf::RenderWindow &window, Core::Config &config, Utility::ResourceManager &resource_manager );
+
+		//! \brief Default deconstructor
 		~GUIManager();
 
 		friend class GUI::Base;
 		friend class GUI::Button;
 		friend class GUI::ActionBar;
 
+		//! \brief Re-positions all GUI elements
+		//! 
+		//! Uses a new resolution to re-position all GUI elements.
+		//! Attempts to place the elements in relation to their old position.
+		//! 
+		//! \param old_resolution_x Previous resolution width
+		//! \param old_resolution_y Previous resolution height
 		void UpdateElements( const float &old_resolution_x, const float &old_resolution_y );
 
-		GUI::Button&	Button( const std::string &name, const std::string &filename = "" );
+		//! \brief Creates or retrieves a button element
+		//! 
+		//! \param name		Unique button name
+		//! \param filename Location of image (in resources/images/)
+		//! 
+		//! \return A newly created or existing button
+		GUI::Button& Button( const std::string &name, const std::string &filename = "" );
+
+		//! \brief Creates or retrieves an action bar element
+		//! 
+		//! \param name		Unique action bar name
+		//! \param filename Location of image (in resources/images/)
+		//! 
+		//! \return A newly created or existing action bar
 		GUI::ActionBar& ActionBar( const std::string &name, const std::string &filename = "" );
 
+		//! \brief Sets the position of an element
+		//! 
+		//! Will not function correctly if two different elements
+		//! have the same unique name.
+		//! 
+		//! \param name Unique element name
+		//! \param x	New x position in pixels
+		//! \param y	New y position in pixels
 		void SetPosition( const std::string &name, const float &x, const float &y );
 
 	protected:
@@ -52,9 +90,15 @@ namespace Core
 		Utility::ResourceManager &resource_manager;	//!< Reference to ResourceManager.
 
 	private:
-		ButtonMap		buttons;		//!< Holds all created buttons.
+		ButtonMap		buttons;	//!< Holds all created buttons.
 		ActionBarMap	actionbars;	//!< Holds all created actionbars.
 	};
 }
 
 #endif // CORE_GUI_MANAGER_H
+
+//! \class Core::GUIManager
+//! \ingroup core
+//! 
+//! Creates and stores graphical user interface elements. Manages their position
+//! upon changes to resolution, attempting to keep the GUI in roughly the same place.
