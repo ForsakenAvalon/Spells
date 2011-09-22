0c4dee2234f13ba3f3e8c23cbdf2e28275a28ca7
James Didehvar <james@didehvar.com>
2011-09-22 16:50:45 +0800

    Documented Utility::Log and made minor changes.
    Removed useless code and/or replaced inefficient code.
    Removed define macros and simply put the value straight into the functions.

--- include/utility/log.h
+++ include/utility/log.h
@@ -1,15 +1,7 @@
+
 #ifndef UTILITY_LOG_H
 #define UTILITY_LOG_H
 
-// Define Max Numerical Write Value
-#define _MAX_LOG_INT_VALUE		1000000
-#define _MAX_LOG_FLOAT_VALUE	1000000
-
-// Include STL
-#include <fstream>
-#include <sstream>
-
-// Include Util
 #include "utility/string.h"
 
 namespace Utility
@@ -17,19 +9,68 @@ namespace Utility
 	class Log
 	{
 		public:
-			//Constructors
-			Log(const char * fileName);
-			//Deconstructors
+			//! \brief Constructor
+			//! 
+			//! Creates a new log file with the specified filename.
+			//! 
+			//! \param filename Name of the file to write to
+			Log( const char *filename );
+
+			//! \brief Default deconstructor
 			~Log();
 
-			void Write(const char * staticText);
-			void Write(float value);
-			void Write(int value);
+			//! \brief Add data to the buffer
+			//! 
+			//! Calling EndLine after writing data will cause
+			//! all data stored since the last EndLine to be
+			//! written to the log file.
+			//! 
+			//! \param staticText Text to add to the buffer
+			//! 
+			//! \see EndLine
+			void Write( const char *staticText );
+
+			//! \brief Add data to the buffer
+			//! 
+			//! Calling EndLine after writing data will cause
+			//! all data stored since the last EndLine to be
+			//! written to the log file.
+			//! 
+			//! \param value Float to add to the buffer
+			//! 
+			//! \see EndLine
+			void Write( float value );
+
+			//! \brief Add data to the buffer
+			//! 
+			//! Calling EndLine after writing data will cause
+			//! all data stored since the last EndLine to be
+			//! written to the log file.
+			//! 
+			//! \param value Integer to add to the buffer
+			//! 
+			//! \see EndLine
+			void Write( int value );
+
+			//! \brief Writes a line
+			//! 
+			//! Causes all data passed into the class through
+			//! Write functions since the last EndLine call
+			//! to be written to the file.
+			//! 
+			//! \see Write
 			void EndLine();
+
 		private:
-			Util::String * name;
-			Util::String * buffer;
+			Utility::String *name;		//!< Stores the name of this file
+			Utility::String *buffer;	//!< Stores the current un-written line
 	};
 }
 
-#endif //UTILITY_LOG_H
+#endif // UTILITY_LOG_H
+
+//! \class Utility::Log
+//! \ingroup utility
+//! 
+//! Opens a file for logging and then provides the functionality to write to it
+//! using a buffer interface.
--- source/utility/log.cpp
+++ source/utility/log.cpp
@@ -1,68 +1,67 @@
+
 #include "utility/log.h"
+
+#include <sstream>
+#include <fstream>
 
 namespace Utility
 {
-	Log::Log(const char * fileName)
+	Log::Log( const char *filename )
 	{
-		name = new String(fileName);
-		buffer = new String;
+		this->name		= new Utility::String(filename);
+		this->buffer	= new Utility::String();
 	}
 
 	Log::~Log()
 	{
-		delete name;
-		delete buffer;
+		delete this->name;
+		delete this->buffer;
 	}
 
-	void Log::Write(const char * staticText)
+	void Log::Write( const char *staticText )
 	{
-		*buffer += staticText;
+		*this->buffer += staticText;
 	}
 
-	void Log::Write(int value)
+	void Log::Write( int value )
 	{
-		// If value is greater than max, print out of range
-		if (value > _MAX_LOG_INT_VALUE) {
-			*buffer += "Value Out Of Range!";
+		// If value is greater than max, print out of range.
+		if ( value > 1000000 )
+		{
+			*this->buffer += "Value out of range!";
+			return;
 		}
-		else {
-			// Convert int to str and write to buffer
-			char * temp = new char[6];
-			_itoa_s(value, temp, 6, 10);
 
-			// Clean up temp before writing to buffer
-			for (uint x = 0; x < 7; x++) {
-				if (temp[x] != '0' && temp[x] != '1' && temp[x] != '2' && temp[x] != '3' && temp[x] != '4' && temp[x] != '5' && temp[x] != '6' && temp[x] != '7' && temp[x] != '8' && temp[x] != '9') {
-					temp[x] = ' ';
-				}
-			}
-
-			for (uint x = 0; x < 7; x++) {
-				*buffer += temp[x];
-			}
-		}
+		// Convert int to string and write to buffer.
+		std::stringstream ss;
+		ss << value;
+		
+		*this->buffer += ss.str();
 	}
 
-	void Log::Write(float value)
+	void Log::Write( float value )
 	{
-		if((value) > _MAX_LOG_FLOAT_VALUE) {
-			*buffer += "Value Out Of Range!";
+		// If value is greater than max, print out of range.
+		if( (value) > 1000000.f ) 
+		{
+			*this->buffer += "Value out of range!";
+			return;
 		}
-		else {
-			std::stringstream ss (std::stringstream::in | std::stringstream::out);
-			ss << value;
-			*buffer += ss.str();
-		}
+
+		// Convert float to string and write to buffer.
+		std::stringstream ss;
+		ss << value;
+
+		*this->buffer += ss.str();
 	}
 
 	void Log::EndLine()
 	{
-		std::fstream file((*name),std::ios_base::app, 64);
+		std::fstream file(*this->name, std::ios_base::app);
 		
-		*buffer += '\n';
+		*this->buffer += '\n';
 
-		file.write(buffer->c_str(),buffer->size());
+		file.write(buffer->c_str(), buffer->size());
 		file.close();
 	}
 }
-
